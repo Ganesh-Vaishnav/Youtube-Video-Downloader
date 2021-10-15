7015df4c8f3c13eeb6ec8f3604c3c4aae965b737
Ganesh <vvaishnav01010@gmail.com>
2021-10-15 21:53:10 +0530

    Explanation to understand the working better

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from pytube import YouTube
-from tkinter import *
-import requests
-from PIL import ImageTk, Image
-SAVE_PATH = "C:\\Users\\DELL_ADMIN\\Downloads"
-
+from pytube import YouTube    # for accessing youtube videos and features
+from tkinter import *         # for GUI Interface
+import requests               # to access the image through url
+from PIL import ImageTk, Image      #to edit the thumb_nail image of video
+SAVE_PATH = "C:\\Users\\DELL_ADMIN\\Downloads"   # video will be saved at this location
+# function for getting an title image of the video to ensure that you are downloading the right video
+'''
+To understand the code better !
+Start through the line 91 first and follow the functions as they are called
+'''
 def get_video_image():
-    get_video_image.file = open("thumb_nail.png", "wb")
-    get_video_image.file.write(start.thumb_nail.content)
+    get_video_image.file = open("thumb_nail.png", "wb")   # create a file for storing the image inside the project
+    get_video_image.file.write(start.thumb_nail.content)    # access the content of the file
     get_video_image.file.close()
+    # for adjusting and editing the image inside the GUI Page
     get_video_image.thumb_nail_image = Image.open("thumb_nail.png")
     get_video_image.thumb_nail_image = get_video_image.thumb_nail_image.resize((150, 150), Image.ANTIALIAS)
     get_video_image.my_image = ImageTk.PhotoImage(get_video_image.thumb_nail_image)
@@ -19,14 +24,14 @@ def start():
     global SAVE_PATH
     mp4_button.config(state = NORMAL)
     mp3_button.config(state=NORMAL)
-    start_button.config(state=DISABLED)
-    url_entry.config(state=DISABLED)
-    start.link = url_entry.get()
+    start_button.config(state=DISABLED)    # user can't click the button again as of now
+    url_entry.config(state=DISABLED)       # user can't write in the entry box as of now
+    start.link = url_entry.get()       # get the link from the entry box
     try :
         start.yt = YouTube(start.link)
-        start.thumb_nail = requests.get(start.yt.thumbnail_url)
-        get_video_image()
-        video_title.config(text = start.yt.title)
+        start.thumb_nail = requests.get(start.yt.thumbnail_url)  # get the link of the thumbnail of the image
+        get_video_image()                              # show the title image i.e. thumbnail of the video
+        video_title.config(text = start.yt.title)     # show the title of the video
         mp4_button.place(x = 550,y = 470)
         mp3_button.place(x=650, y=470)
     except Exception as e:
@@ -35,7 +40,7 @@ def start():
 
     finally:
         try_again_button.config(state = NORMAL)
-
+# download the video(mp4)
 def download_mp4():
     download_mp4.file_name = ''.join(char for char in start.yt.title if char.isalnum() or char == ' ')
     try:
@@ -52,13 +57,14 @@ def download_mp4():
         print(str(e))
     mp4_button.config(state = DISABLED)
     mp3_button.config(state=DISABLED)
+# to download as audio(mp3)
 def download_mp3():
-
+    download_mp3.file_name = ''.join(char for char in start.yt.title if char.isalnum() or char == ' ')
     try:
         download_mp3.mp3files = start.yt.streams.filter(progressive=True,
                                               file_extension="mp4") \
             .last(). \
-            download(output_path=SAVE_PATH, filename=start.yt.title+".mp3")
+            download(output_path=SAVE_PATH, filename=download_mp3.file_name+".mp3")
         final_label.config(text="Your audio has been downloaded at "+SAVE_PATH,
                            fg="White")
     except Exception as e:
@@ -67,6 +73,7 @@ def download_mp3():
         print(str(e))
     mp4_button.config(state=DISABLED)
     mp3_button.config(state = DISABLED)
+# to clear all previous operation
 def try_again():
     thumb_image.place_forget()
     start_button.config(state = NORMAL)
@@ -80,20 +87,15 @@ def try_again():
     mp4_button.place_forget()
     mp3_button.place_forget()
 
-window = Tk()
 
-window.geometry("1300x700+0+0")
+window = Tk()
+window.geometry("1300x700+0+0")  # size of the page
 window.title("YouTube Video Downloader  ")
 window.config(background="Black")
-icon = PhotoImage(file="youtube photo.png")
+icon = PhotoImage(file="youtube photo.png")  # for title image of the GUI Page
 yt = PhotoImage(file="YT.png")
-
-
-window.iconphoto(True, icon)
-
-#canvas = Canvas(window,width = 200,height = 200,bg = "black")
-#canvas.create_image(100,150,image = thumb_nail_image)
-#canvas.place(x=100,y=390)
+window.iconphoto(True, icon)   # set the title of the image
+# Label
 title = Label(window,
                   text="        YOUTUBE VIDEO DOWNLOADER  ",
                   font=("Classic", 30, "bold"),
@@ -104,7 +106,7 @@ title = Label(window,
                   )
 
 title.place(x=100, y=30)
-
+# Label
 paste = Label(window,
                   text="Paste your URL link Here ⬇ ",
                   font=("Classic", 20, "bold"),
@@ -113,21 +115,23 @@ paste = Label(window,
 
                   )
 paste.place(x=100, y=250)
+# Label
 download_place = Label(window,
-                     text="Confirm your video name Here  ",
+                     text="Confirm your video  Here  ",
                      font=("Classic", 20, "bold"),
                      background="Black",
                      foreground="Green",
 
                      )
 download_place.place(x=100, y=350)
+# Entry to enter the link of the url
 url_entry = Entry(window,
                       font=("", 18, ""),
                       width=60,
                       background="Silver",
-                      #state = ACTIVE
                   )
 url_entry.place(height=50, x=100, y=300)
+# Button to start accessing for the name and other details of the video
 start_button = Button(window,
                           text="START",
                           font=("", 18, "bold"),
@@ -135,8 +139,7 @@ start_button = Button(window,
                           fg="White",
                           activeforeground="White",
                           activebackground="Black",
-                          #state = ACTIVE,
-                          command=start
+                          command=start    # refer the start function
                           )
 start_button.place(x=860, y=300)
 video_title = Label(window,
